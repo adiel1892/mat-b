c6d063c8a5d7ca2248221a53cb029c2aa9cff9db
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-13 13:50:45 +0200

    final

--- mat.hpp
+++ mat.hpp
@@ -2,7 +2,7 @@
 using namespace std;
 namespace ariel{
     string mat(int cols, int rows, char symbol1, char symbol2);
-    string matrixToString(vector<vector<char>> matrix, int cols);
+    string matrixToString(vector<vector<char> > matrix, int cols);
     void checkInput1(int rows, int cols);
     void checkInput2(char symbol1, char symbol2);
     
